f3157ac2b7d546f4312a5b029ae5b7e384dd286b
nayanprakash06 <124758491+nayanprakash06@users.noreply.github.com>
2026-03-16 13:11:29 +0530

    Add files via upload

---
+++ Python Codes/Week10/Bluetooth/Bluetooth_1_RFConnect.py
@@ -0,0 +1,61 @@
+from machine import Pin
+import bluetooth
+import time
+
+ble = bluetooth.BLE()
+ble.active(True)
+
+# UUIDs
+SERVICE_UUID = bluetooth.UUID("6e400001-b5a3-f393-e0a9-e50e24dcca9e")
+CHAR_UUID = bluetooth.UUID("6e400002-b5a3-f393-e0a9-e50e24dcca9e")
+
+# Characteristic only needs WRITE
+CHAR = (CHAR_UUID, bluetooth.FLAG_WRITE)
+
+SERVICE = (SERVICE_UUID, (CHAR,),)
+
+((char_handle,),) = ble.gatts_register_services((SERVICE,))
+
+connections = set()
+
+def irq(event, data):
+    
+    global connections
+    # Event 1 → Phone Connected
+    if event == 1:
+        conn_handle, addr_type, addr = data
+        connections.add(conn_handle)
+        print("Phone connected")
+    
+    # Event 2 → Phone Disconnected
+    elif event == 2:
+        conn_handle, addr_type, addr = data
+        connections.remove(conn_handle)
+        print("Phone disconnected")
+        advertise()
+        
+    # Event 3 → phone wrote data
+    elif event == 3:
+        conn_handle, value_handle = data
+
+        if value_handle == char_handle:
+
+            msg = ble.gatts_read(char_handle).decode().strip()
+
+            print("Received from phone:", msg)
+            
+
+ble.irq(irq)
+
+
+def advertise():
+    ble.config(gap_name="ESP32-Nayan")
+    ble.gap_advertise(100)
+
+
+advertise()
+
+print("Waiting for phone connection...")
+
+while True:
+    time.sleep(1)
